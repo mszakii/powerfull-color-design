a1bf3fbb114c187f60b502b7da56c892cbabf71f
Mohammed Zaky <62839447+mohammed-zaky@users.noreply.github.com>
2021-08-22 11:58:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// declaration
+let up = document.querySelector(".up");
+let nameEl = document.querySelector("h1");
+
+// scroll detector
+window.onscroll = _ => 
+this.scrollY >= 750?
+up.style.right = "20px":
+up.style.right = "-100%";
+
+// func
+up.onclick = _ => this.location.href = "#header";
+
+
+// accipt name
+if (localStorage.name === "undefined") {
+  var userName = window.prompt("what's your first name?");
+}
+
+nameEl.innerHTML = `Hello ${userName}.`;
+console.log(`Hello ${userName} 😁`);
+console.log("what do you do in console?");
